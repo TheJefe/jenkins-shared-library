6ab72d80564fb1634727b9798d394667d2fae9b5
Jeff Koenig <jkoenig311@gmail.com>
2018-04-30 00:03:20 -0400

    Add the region parameter

--- src/ecs.groovy
+++ src/ecs.groovy
@@ -1,9 +1,10 @@
-def deployService(cluster, service){
+def deployService(cluster, service, region){
     env.CLUSTER = cluster
     env.SERVICE = service
+    env.REGION = region
     withCredentials([usernamePassword(credentialsId: 'ecs', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
         sh '''#!/bin/bash -l
-        aws ecs update-service --force-new-deployment --cluster ${CLUSTER} --service ${SERVICE}
+        aws ecs update-service --force-new-deployment --cluster ${CLUSTER} --service ${SERVICE} --region ${REGION}
         '''
     }
 }
